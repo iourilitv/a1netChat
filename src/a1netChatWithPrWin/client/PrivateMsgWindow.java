a3f8c156fcd755e21d6e97290e3c6033297329a6
iourilitv <iourilitv1965@gmail.com>
2019-08-23 16:05:48 +0300

    Deleted not necessary comments and code. Two more errors have happened in IDEA: 	- 2 onAction="#sendPrivateMsg"(btn and testfield) в privateMsgWindow.fxml became red (IDEA recognizes it as a troubles) and sendPrivateMsg method in Controller became not active. But, the app works correctly! Tried to comment sendPrivateMsg method in Controller or/and onAction="#sendPrivateMsg", but it doesn't work. All works correctly. The error below does not appears. In the client(with private message window) console shows: Отправляем сообщение о закрытии Exception in thread "Thread-3" java.lang.IncompatibleClassChangeError. Probably the problem with static type of prMsgWindow in Controller.

--- src/a1netChatWithPrWin/client/PrivateMsgWindow.java
+++ src/a1netChatWithPrWin/client/PrivateMsgWindow.java
@@ -9,35 +9,10 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 
 public class PrivateMsgWindow extends Stage {
-
-    //TODO pr.window opening.Deleted
-    //String chatCompanionNick;//кому
-    //TODO pr.window opening.Added
     String nick;//имя партнера (кому/от кого)
     String msg;
-
     DataOutputStream out;
 
-    //TODO pr.window opening.Deleted
-    //VBox prVBoxChat;//TODO Не помогло.Удалить
-
-    //TODO pr.window opening.Deleted
-    /*public PrivateChatStage(Controller controller, VBox prVBoxChat) throws IOException {
-        this.chatCompanionNick = controller.getChatCompanionNick();
-        this.out = controller.getOut();
-        this.prVBoxChat = prVBoxChat;//TODO Не помогло.Удалить
-
-        FXMLLoader loader = new FXMLLoader();
-        Parent root = loader.load(getClass().getResourceAsStream("privateChat.fxml"));
-        setTitle("Private chat with " + chatCompanionNick);
-        Scene scene = new Scene(root, 300, 400);
-        setScene(scene);
-
-        //TODO временно.
-        System.out.println("PrivateChatStage.chatCompanionNick: " + chatCompanionNick);
-
-    }*/
-    //TODO pr.window opening.Added
     public PrivateMsgWindow(Controller controller, String nick, String msg) throws IOException {
         this.nick = nick;
         this.msg = msg;
